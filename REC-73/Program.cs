f74eaa4981a4faaa2dd02fdb8063aa870d5834f6
сергей токарев <sseregatokarew@gmail.com>
2021-11-18 15:04:47 +0700

    Добавьте файлы проекта.

---
+++ REC-73/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace REC_73
+{
+    class Program
+    {
+        static float MinePow(float number)
+        {
+            return number * number;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число , которое необходимо возвести в квадрат: ");
+            var Num = float.Parse(Console.ReadLine());
+            Console.Write($"Квадрат введенного числа: {MinePow(Num)} ");
+            Console.ReadKey();
+        }
+    }
+}
